1da0cdf3b2d8fd1362fc3e39b82455b9d4229d15
Casneil Simpson <46893725+Casneil@users.noreply.github.com>
2019-08-25 10:37:29 +0200

    Add files via upload

---
+++ Homework-ReDI/Hello_World.java
@@ -0,0 +1,10 @@
+package stars;
+
+public class Hello_World {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World, Welcome to ReDI School ");
+	}
+
+}
---
+++ Homework-ReDI/Stars.java
@@ -0,0 +1,51 @@
+package stars;
+import java.util.Scanner;
+
+
+public class Stars {
+	
+	public static void I() {
+		System.out.print("I");
+	}
+
+	public static void M() {
+		System.out.println("\nM");
+	}
+	
+	public static String Pattern() {
+		String pattern = "YX"; 
+		StringBuilder sb = new StringBuilder();
+		int count = (int) (Math.random() * pattern.length());
+		sb.append(pattern.charAt(count));
+		return sb.toString();
+	}
+	
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("Please give me number of rows");
+		
+		int rows = scan.nextInt();
+		
+		System.out.print("Please give me number of columns");
+		
+		int columns = scan.nextInt();
+
+		for(int i = 1; i < rows; i++) {
+			System.out.println();
+			for(int j = rows; j >=columns; j--) {
+				
+				if (i == 1) {
+					I();
+				}else 
+//				System.out.print("#");	
+				System.out.print(Pattern());
+			}
+			
+			columns--;
+		}
+		M();
+		
+	}
+}
